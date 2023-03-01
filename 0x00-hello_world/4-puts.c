d30dd986268c2b42e8e96ee8ef0dfc86be6bf690
TheRealEV <ev@linux.com>
2023-03-01 06:08:35 -0800

    hello

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+/*Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.*/
+
+#include <stdio.h>
+int main()  
+{
+    
+    char str[] = "Programming is like building a multilingual puzzle\n";
+    puts(str);
+    
+    return 0;
+    
+}
