166686cfce525acde091aa8477d9657820fbc2bb
TheRealEV <ev@linux.com>
2023-03-01 06:29:36 -0800

    Awva

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,14 @@
-/*Write a C program that prints exactly with proper grammar,
- *  but the outcome is a piece of art,, followed by a new line.*/
-
+/**
+*main - entry point
+*
+*Return: Always 0
+*
+*/
 #include <stdio.h>
 int main(void)
 {
- 
-printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-return 0;
+	return (0);
+
 }
