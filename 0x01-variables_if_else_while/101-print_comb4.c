9a7d91a1e8ec4b1a4bef9a08b1400badfb1b4b71
TheRealEV <ev@linux.com>
2023-03-01 14:38:38 -0800

    not easy

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry Point
+ * Description: Prints all possible different combinations of 3
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int c, i, k;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		for (i = '0'; i <= '9'; i++)
+		{
+			for (k = '0'; k <= '9'; k++)
+			{
+				if (c < i && i < k)
+				{
+					putchar(c);
+					putchar(i);
+					putchar(k);
+
+					if (c != '7')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
