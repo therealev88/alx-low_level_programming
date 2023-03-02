143c7a6cf2f21bd57f64f41a976075897a490e0c
TheRealEV <ev@linux.com>
2023-03-02 04:25:44 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add- Entry point
+ * Description: A function that adds two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: Sum of intege
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = x + y;
+	return (sum);
+}
