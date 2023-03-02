b7e2e186cfd6df8e4b5f7ea57c37c3a90f46840d
TheRealEV <ev@linux.com>
2023-03-02 02:05:22 -0800

    Wahala

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include <unistd.h>
+#include "main.h"
 /**
  * main - Entry point
  * Description: Print putchar
@@ -7,16 +6,14 @@
  */
 int main(void)
 {
-	char str[10] = "_putchar";
-
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
-	return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return  (0);
 }
