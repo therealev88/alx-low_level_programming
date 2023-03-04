719bad3691726e388c65e816e80333a48cf118ce
TheRealEV <ev@ubuntu.com>
2023-03-04 14:57:21 +0100

    update

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main- Entry point
+ * Description: a function that checks if an integer is + or -
+ * Return: Point of no return (void)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
