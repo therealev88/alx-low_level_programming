efb17590362ec0137b87382341d24398521b1ef0
TheRealEV <ev@ubuntu.com>
2023-03-06 22:56:48 +0100

    crackhead

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,18 +1,36 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * *_strpbrk - function declaration
+ * @s: string to search
+ * @accept: bytes to search
+ * Return: pointer or NULL
  */
-int main(void)
-{
-	char *s = "hello, world";
-	char *f = "world";
-	char *t;
 
-	t = _strpbrk(s, f);
-	printf("%s\n", t);
+char *_strpbrk(char *s, char *accept);
+
+/**
+ * *_strpbrk - function definition
+ * @s: the string to be searched
+ * @accept: bytes to search for
+ * Description: searches for a string of a set of bytes
+ * Return: pointer to matched byte or NULL if no match
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int str_index;
+	int sub_str_index;
+
+	for (str_index = 0; s[str_index] != '\0'; str_index++)
+	{
+		for (sub_str_index = 0; accept[sub_str_index] != '\0'; sub_str_index++)
+		{
+			if (s[str_index] == accept[sub_str_index])
+			{
+				return (s + str_index);
+			}
+		}
+	}
 	return (0);
 }
